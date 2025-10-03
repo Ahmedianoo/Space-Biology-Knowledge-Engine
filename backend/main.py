93b5edca14fcd1d178901a32bc396e8d847d031c
hazemyasser123 <153863853+hazemyasser123@users.noreply.github.com>
2025-10-03 16:45:47 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],   # Allow access from anywhere
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get('/')
+@app.get("/")
 def hello_world():
-    return {"message": "hello world"}
+    return {"message": "zuma and niazi are fucking the back"}
